d9d6e97854f7de6c7b358df7a1325758531c05d3
anuragmuglikar <anuragm11@gmail.com>
2018-09-13 18:24:19 -0400

    playMusic with ISR OVERFLOW Flag

--- functions.h
+++ functions.h
@@ -17,27 +17,35 @@ void timer0_init()
 {
 	TCCR0B |= (1 << CS01);	// timer with no pre-scaling
 	TCNT0 = 0;		// start counter
+	TIMSK0 = 0b00000001; //Enable overflow interrupt.
 }
 
 void playMusic(){
-	if (TCNT0 >= 250)
-	{
 		PORTD ^= (1 << 6);
-		TCNT0 = 0;            // reset counter
-	}
+		//TCNT0 = 0;            // reset counter
+		
 }
 
 void init_interrupts(){
 		
 	TCCR1B |= 0b00000001; //setting the prescaler to 1.
 	TCCR1B |= 0b00000000; //Enabling ICES1 (7) to detect a falling edge
-	TIMSK1 = 0b00100000; //Enable ICEI pin
+	TIMSK1 = 0b00100000; //Enable ICEI pin.
 	sei();				// Enable global interrupt
-	
 
 }
 
 ISR(TIMER1_CAPT_vect){
-	PORTB ^= 1 << 5;
-	TCCR1B ^= 1 << 6;	//setting ICES Pin to detect a rising edge so that the same pin can be toggle back when button is released.
+	//PORTB ^= 1 << 5;
+	//TCCR1B ^= 1 << 6;	//setting ICES Pin to detect a rising edge so that the same pin can be toggle back when button is released.
+}
+
+ISR(TIMER0_OVF_vect){
+	buzzAtCount ++;
+	if(buzzAtCount == 80){
+		buzzAtCount = 0;
+		playMusic();			//method for buzzing the buzzer.
+	}
+	TIFR0 = 1 << 0;
+	TCNT0 = 0;
 }
--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include "functions.h"
 
+
+int buzzAtCount = 0;
+#include "functions.h"
 
 void init(){
 	DDRB = 0x20;	//0b00100000; PB5 => Output // Setting the input high on DDR makes the pin OP.
 	PORTB = 0x01;	//Writing something on the input pins, will take them to internal pull up.
 	DDRD = 0b01000000;
-	PORTB |= 0b00000000;
 	timer0_init();
 	init_interrupts();
 }
@@ -20,7 +21,7 @@ int main(void)
 	while (1)
 	{
 		pollButton();		//method for polling input pin without interrupts
-		playMusic();			//method for buzzing the buzzer.
+		
 		
 	}
 }
